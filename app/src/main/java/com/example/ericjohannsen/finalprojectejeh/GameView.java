9fb530db0440f3ccea85fd64936e15d871a2811a
EJohannsen <ejohannsen7@gmail.com>
2015-04-02 15:40:04 -0500

    Added some stuff

--- app/src/main/java/com/example/ericjohannsen/finalprojectejeh/GameView.java
+++ app/src/main/java/com/example/ericjohannsen/finalprojectejeh/GameView.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
@@ -17,18 +18,25 @@ import android.view.SurfaceView;
 public class GameView extends SurfaceView{
     private Bitmap bmp;
     private Bitmap bmp2;
+    private Bitmap bitmap;
     private SurfaceHolder holder;
     private GameLoopThread gameLoopThread;
     private MainActivity activity;
 
     private static float XVelocity = 0;
     private static float YVelocity = 0;
-    private static float curX = 200;
-    private static float curY = 300;
+    private static float curX = 300;
+    private static float curY = 500;
+    private static int width;
+    private static int height;
 
     public GameView(Context context) {
         super(context);
         activity = (MainActivity) this.getContext();
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        height = displaymetrics.heightPixels;
+        width = displaymetrics.widthPixels;
         gameLoopThread = new GameLoopThread(this);
         holder = getHolder();
         holder.addCallback(new SurfaceHolder.Callback() {
@@ -58,22 +66,29 @@ public class GameView extends SurfaceView{
         //load images into memory here
         bmp = BitmapFactory.decodeResource(getResources(), R.drawable.smallcircle);
         bmp2 = BitmapFactory.decodeResource(getResources(), R.drawable.circle);
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pause);
     }
 
     //fling event
     public static void handleFling(MotionEvent event1, MotionEvent event2, float xVel, float yVel)
     {
-        XVelocity = (event2.getX() - event1.getX()) / (event1.getDownTime() / 500000);
-        YVelocity = (event2.getY() - event1.getY()) / (event1.getDownTime() / 500000);
+        Log.d("Width:", String.valueOf(width));
+        Log.d("Height", String.valueOf(height));
+        XVelocity = (event2.getX() - event1.getX()) / (event1.getDownTime() / 16000);
+        YVelocity = (event2.getY() - event1.getY()) / (event1.getDownTime() / 16000);
     }
 
     //for now reset with single tap
     public static void handleSingleTapUp(MotionEvent event)
     {
+        if (event.getX() > width-(width/8.5) && event.getY() < (height/13)) {
+            Log.d("Pause", "Pause Menu");
+        }
+
         XVelocity = 0;
         YVelocity = 0;
-        curX = 200;
-        curY = 300;
+        curX = 300;
+        curY = 500;
     }
 
     protected void Draw(Canvas canvas) {
@@ -91,8 +106,6 @@ public class GameView extends SurfaceView{
         //so (150, 150) is center of this circle
         canvas.drawBitmap(bmp2, 100, 100, null);
         canvas.drawBitmap(bmp, curX, curY, null);
+        canvas.drawBitmap(bitmap, width-80, 0, null);
     }
-
-
-
 }
